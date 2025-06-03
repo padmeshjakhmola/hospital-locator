fcb19b124048a128b0d1b279a916835ebb3dcba1
Padmesh Jakhmola <prankshadow@gmail.com>
2025-06-03 14:34:03 +0530

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,13 @@
 import GlobalProviders from "@/lib/global-provider";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   return (
     <GlobalProviders>
       <Stack screenOptions={{ headerShown: false }} />
